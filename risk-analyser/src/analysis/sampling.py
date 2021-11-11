9e1cdb0d8876a7b2e0b613f723eeeb110883327e
Rheddes <rens.h@me.com>
2021-11-11 17:36:06 +0100

    Use reduced_callgraphs dir and ignore

--- risk-analyser/src/analysis/sampling.py
+++ risk-analyser/src/analysis/sampling.py
@@ -110,7 +110,7 @@ def proportional_risk(graph: RiskGraph, risk_scores):
 
 if __name__ == '__main__':
     list_of_lists = []
-    for file in glob.glob(os.path.join(config.BASE_DIR, 'repos', '**', '*-reduced.json'), recursive=True):
+    for file in glob.glob(os.path.join(config.BASE_DIR, 'reduced_callgraphs', '**', '*-reduced.json'), recursive=True):
         name = file.split('/')[-1]
         graph = RiskGraph.create(*parse_JSON_file(file), auto_update=False)
 
